5ad3788e5bf29550ed79e94994d23e47c5116a85
redrafael08 <204200@stmichaelcollege.nl>
2025-01-25 21:52:10 +0100

    cache

--- Untitled-1 copy.py
+++ Untitled-1 copy.py
@@ -6,14 +6,14 @@ import random
 
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 600))
+screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 screenDistance = 400
 screenWidth, screenHeight = screen.get_size()
 screenCenter = (screenWidth / 2, screenHeight / 2)
 
 
 playerPos = [200, 500, 200]
-playerSpeed = 0.1
+playerSpeed = 0.1*10
 playerAngle = [0, 0]
 
 
@@ -24,10 +24,10 @@ font = pygame.font.Font(None, 50)
 pygame.mouse.set_visible(False)
 
 
-for z in range(40):
+for z in range(20):
     row = []
-    for x in range(40):
-        if (x+z)%2 == 1:
+    for x in range(20):
+        if (x+z)%1 == 0:
             row.append(1)
         else:
             row.append(0)
@@ -79,7 +79,7 @@ def project(point):
 veldown = 0
 
 while True:
-    clock.tick(60)
+    clock.tick(30)
     screen.fill((255, 255, 255))
 
 
@@ -93,13 +93,13 @@ while True:
 
 
     if keys[pygame.K_LEFT]:
-        playerAngle[0] += math.radians(0.1) * currentframe
+        playerAngle[0] += math.radians(1)
     if keys[pygame.K_RIGHT]:
-        playerAngle[0] -= math.radians(0.1) * currentframe
+        playerAngle[0] -= math.radians(1)
     if keys[pygame.K_UP]:
-        playerAngle[1] += math.radians(0.1) * currentframe
+        playerAngle[1] += math.radians(1)
     if keys[pygame.K_DOWN]:
-        playerAngle[1] -= math.radians(0.1) * currentframe
+        playerAngle[1] -= math.radians(1)
 
     if playerAngle[1] > math.radians(90):
         playerAngle[1] = math.radians(90)
@@ -110,24 +110,24 @@ while True:
     sinb, cosb = math.sin(playerAngle[1]), math.cos(playerAngle[1])
 
     if keys[pygame.K_a]:
-        playerPos[0] -= playerSpeed * cosa * currentframe
-        playerPos[2] -= playerSpeed * sina * currentframe
+        playerPos[0] -= playerSpeed * cosa 
+        playerPos[2] -= playerSpeed * sina 
     if keys[pygame.K_d]:
-        playerPos[0] += playerSpeed * cosa * currentframe
-        playerPos[2] += playerSpeed * sina * currentframe
+        playerPos[0] += playerSpeed * cosa 
+        playerPos[2] += playerSpeed * sina 
     if keys[pygame.K_w]:
-        playerPos[0] += playerSpeed * math.cos(playerAngle[0] + math.radians(90)) * currentframe
-        playerPos[2] += playerSpeed * math.sin(playerAngle[0] + math.radians(90)) * currentframe
+        playerPos[0] += playerSpeed * math.cos(playerAngle[0] + math.radians(90)) 
+        playerPos[2] += playerSpeed * math.sin(playerAngle[0] + math.radians(90)) 
     if keys[pygame.K_s]:
-        playerPos[0] -= playerSpeed * math.cos(playerAngle[0] + math.radians(90)) * currentframe
-        playerPos[2] -= playerSpeed * math.sin(playerAngle[0] + math.radians(90)) * currentframe
+        playerPos[0] -= playerSpeed * math.cos(playerAngle[0] + math.radians(90)) 
+        playerPos[2] -= playerSpeed * math.sin(playerAngle[0] + math.radians(90)) 
     if keys[pygame.K_SPACE] and playerPos[1] == 20:
         veldown = -2.5
         playerPos[1] = 21
 
     
     playerPos[1] -= veldown
-    veldown += 0.01 * clock.get_rawtime()
+    veldown += 0.01 
 
     if playerPos[1] <= 20:
         playerPos[1] = 20
@@ -139,9 +139,15 @@ while True:
 
     polygons = []
 
+    cache = {}
+
+    a = 0
+
     for z, row in enumerate(tiles):
         for x, column in enumerate(row):
+            
             if column == 1:
+                
                 tile = [[x*gridSize,0,z*gridSize],
                         [x*gridSize,0,(z+1)*gridSize],
                         [(x+1)*gridSize,0,(z+1)*gridSize],
@@ -150,15 +156,28 @@ while True:
                 points = [rotate(point) for point in tile]
                 polygon = []
                 for point in points:
-                    if point[2] > 0:
-                        polygon.append(project(point))
+                    tpoint = tuple(point)
+                    if tpoint in cache:
+                        polygon.append(cache[tpoint])
+
+
                     else:
-                        point2 = points[points.index(point) - 1]
-                        if point2[2] > 0:
-                            polygon.append(project(line_intersection(point, point2)))
-                        point2 = points[(points.index(point) + 1) % len(points)]
-                        if point2[2] > 0:
-                            polygon.append(project(line_intersection(point, point2)))
+
+                        a +=1
+                        if point[2] > 0:
+                            ppoint = project(point)
+                            polygon.append(ppoint)
+                            cache[tpoint] = ppoint
+
+                        else:
+                            point2 = points[points.index(point) - 1]
+                            if point2[2] > 0:
+                                polygon.append(project(line_intersection(point, point2)))
+
+                            point2 = points[(points.index(point) + 1) % len(points)]
+                            if point2[2] > 0:
+                                polygon.append(project(line_intersection(point, point2)))
+
                 if len(polygon)>2:
                     maxx = max(polygon, key=lambda x: x[0])[0]
                     minx = min(polygon, key=lambda x: x[0])[0]
@@ -177,9 +196,10 @@ while True:
 
     text = font.render(f"{round(clock.get_fps())}", True, (0, 0, 0))
     screen.blit(text, (100, 100))
-    text = font.render(f"{len(polygons)}", True, (0, 0, 0))
+    text = font.render(f"{playerPos}", True, (0, 0, 0))
     screen.blit(text, (100, 200))
-
+    text = font.render(f"{a}", True, (0, 0, 0))
+    screen.blit(text, (100, 300))
     pygame.display.update()
 
 
