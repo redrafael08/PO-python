edb734d67e60a13354837c21512ab97c2331cb46
redrafael08 <204200@stmichaelcollege.nl>
2025-01-27 09:33:32 +0100

    crosshair color

--- Untitled-1 copy.py
+++ Untitled-1 copy.py
@@ -238,7 +238,7 @@ while True:
                             ppoint = project(point)
                             polygon.append(ppoint)
                             cache[tpoint] = ppoint
-                            a +=1
+                            a += 1
 
                         else:
                             point2 = points[points.index(point) - 1]
@@ -285,10 +285,11 @@ while True:
                 pygame.draw.circle(screen, (0,0,0), pprojectile, 2/rprojectile[2]*screenDistance)
 
 
-    pygame.draw.line(screen, (0,255,0), (screenCenter[0]-15,screenCenter[1]), (screenCenter[0]-5,screenCenter[1]),2)
-    pygame.draw.line(screen, (0,255,0), (screenCenter[0],screenCenter[1]-15), (screenCenter[0],screenCenter[1]-5),2)
-    pygame.draw.line(screen, (0,255,0), (screenCenter[0]+15,screenCenter[1]), (screenCenter[0]+5,screenCenter[1]),2)
-    pygame.draw.line(screen, (0,255,0), (screenCenter[0],screenCenter[1]+15), (screenCenter[0],screenCenter[1]+5),2)
+
+    pygame.draw.line(screen, (0,200,0), (screenCenter[0]-15,screenCenter[1]), (screenCenter[0]-5,screenCenter[1]),2)
+    pygame.draw.line(screen, (0,200,0), (screenCenter[0],screenCenter[1]-15), (screenCenter[0],screenCenter[1]-5),2)
+    pygame.draw.line(screen, (0,200,0), (screenCenter[0]+15,screenCenter[1]), (screenCenter[0]+5,screenCenter[1]),2)
+    pygame.draw.line(screen, (0,200,0), (screenCenter[0],screenCenter[1]+15), (screenCenter[0],screenCenter[1]+5),2)
 
 
 
