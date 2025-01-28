b7368dc6426feaeb961c01ab70f1809dd5a06c10
redrafael08 <204200@stmichaelcollege.nl>
2025-01-28 18:59:12 +0100

    online test

---
+++ server.py
@@ -0,0 +1,40 @@
+import socket
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("192.168.1.25", 5555))
+
+s.listen(2) # maximale aantal clienten
+
+
+client, addr = s.accept()
+print('client1 connected')
+client2, addr2 = s.accept()
+print('client2 connected')
+while True:
+    data = client.recv(4096)
+    client2.send(data)
+    data = client2.recv(4096)
+    client.send(data)
+    
+
+
+'''
+# https://stackoverflow.com/questions/38412887/how-to-send-a-list-through-tcp-sockets-python
+y = [0,12,6,8,3,2,10] 
+# Convert To String
+y = str(y)
+# Encode String
+y = y.encode()
+# Send Encoded String version of the List
+s.send(y)
+'''
+'''
+data = connection.recv(4096)
+# Decode received data into UTF-8
+data = data.decode('utf-8')
+# Convert decoded data into list
+data = eval(data)
+'''
+
