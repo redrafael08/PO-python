f7e30acfe1c4c73df22b36c35465c7d2af28e9ae
redrafael08 <204200@stmichaelcollege.nl>
2025-02-03 17:14:32 +0100

    gui

---
+++ gui.py
@@ -0,0 +1,58 @@
+import math
+import pygame
+from sys import exit
+import random
+import numpy as np
+
+
+pygame.init()
+screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+screenWidth, screenHeight = screen.get_size()
+screenCenter = (screenWidth / 2, screenHeight / 2)
+
+bg = pygame.image.load('background.png')
+
+font = pygame.font.Font(None, 100)
+
+def Button(pos,size,text,command):
+    buttons.append([pos[0]-size[0]/2, pos[1]-size[1]/2, pos[0]+size[0]/2, pos[1]+size[1]/2, command])
+    buttonsurface = pygame.Surface(size, pygame.SRCALPHA)
+    pygame.draw.rect(buttonsurface, (255,255,255,175), (0,0,size[0],size[1]))
+    screen.blit(buttonsurface, (pos[0]-size[0]/2, pos[1]-size[1]/2))
+    pygame.draw.rect(screen, (0,200,0), (pos[0]-size[0]/2,pos[1]-size[1]/2,size[0],size[1]), 5)
+    text = font.render(f"{text}", True, (0, 200, 0))
+    screen.blit(text, (pos[0]-text.get_width()/2, pos[1]-text.get_height()/2))
+
+
+
+buttons = []
+
+clock = pygame.time.Clock()
+while True:
+    clock.tick(60)
+    screen.fill((255, 255, 255))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse = pygame.mouse.get_pos()
+
+            for button in buttons:
+                if button[0] < mouse[0] < button[2] and button[1] < mouse[1] < button[3]:
+                    button[4]()
+
+   
+
+
+    screen.blit(bg, (0,0))
+
+    Button([screenCenter[0],screenCenter[1]],[500,100], 'Singleplayer',lambda: print('he'))
+    Button([screenCenter[0],screenCenter[1]+150],[500,100], 'Multiplayer',lambda: print('a'))
+    Button([screenCenter[0],screenCenter[1]+300],[500,100], 'Quit', lambda: exit())
+
+
+
+    pygame.display.update()
